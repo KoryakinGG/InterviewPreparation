f89b92e37a02afe726f0fb9060258fcab85d1368
KoryakinGG <koryaking@mail.ru>
2021-02-11 19:58:31 +0300

    Я зря сделал АтомикИнтеджер, т.к. он сам по себе работает синхронизатором. Переделал с внутренним классом, но не понял, почему для переменной нужен именно внутренний класс. Если просто добавить поле count в psvm, то пишет, что нужен внутренний класс, и не дает его инкрементить.

--- src/ru/koryaking/thirdhomework/counter/CounterMain.java
+++ src/ru/koryaking/thirdhomework/counter/CounterMain.java
@@ -1,22 +1,45 @@
 package ru.koryaking.thirdhomework.counter;
 
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class CounterMain {
 
-    public static void main(String[] args) throws InterruptedException {
-        ReentrantLock reentrantLock = new ReentrantLock();
-        AtomicInteger atomicInteger = new AtomicInteger(0);
+    static class InnerClass {
+        public int getCount() {
+            return count;
+        }
 
+        int count = 0;
+
+        Lock lock;
+
+        public InnerClass(Lock lock) {
+            this.lock = lock;
+        }
+
+        public void increment () {
+            try {
+                lock.lock();
+                count++;
+            } finally {
+                lock.unlock();
+            }
+
+        }
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Lock reentrantLock = new ReentrantLock();
+        InnerClass innerClass = new InnerClass(reentrantLock);
         new Thread(new Runnable() {
             @Override
             public void run() {
                 for (int i = 0; i <5; i++) {
-                    reentrantLock.lock();
-                    atomicInteger.incrementAndGet();
-                    System.out.println(atomicInteger);
-                    reentrantLock.unlock();
+                    innerClass.increment();
+                    System.out.println(innerClass.getCount());
                 }
             }
         }).start();
@@ -25,10 +48,8 @@ public class CounterMain {
             @Override
             public void run() {
                 for (int i = 0; i <5; i++) {
-                    reentrantLock.lock();
-                    atomicInteger.incrementAndGet();
-                    System.out.println(atomicInteger);
-                    reentrantLock.unlock();
+                    innerClass.increment();
+                    System.out.println(innerClass.getCount());
                 }
             }
         }).start();
