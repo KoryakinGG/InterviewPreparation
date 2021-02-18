89b6ab6d77ef9605511f3c4a15010695f42df169
KoryakinGG <koryaking@mail.ru>
2021-02-18 21:04:49 +0300

    5 домашка

---
+++ src/ru/koryaking/fifthhomework/CrudOperation.java
@@ -0,0 +1,80 @@
+package ru.koryaking.fifthhomework;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.NoSuchElementException;
+
+
+public class CrudOperation {
+
+    private static SessionFactory factory;
+
+    public static void main(String[] args) {
+        try {
+            init();
+//            createStudent("Mark", "Dark");
+//            readStudent(4L);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            shutdown();
+        }
+    }
+
+    public static void init() {
+        PrepareDataApp.forcePrepareData();
+        factory = new Configuration()
+                .configure("configs/hibernate.cfg.xml")
+                .buildSessionFactory();
+    }
+
+    public static void createStudent(String name, String mark) {
+        try (Session session = factory.getCurrentSession()) {
+            session.beginTransaction();
+            Student student = new Student(name, mark);
+            session.save(student);
+            session.getTransaction().commit();
+            System.out.println("Student create " + student);
+        }
+    }
+
+
+    public static void readStudent(Long id) {
+        try (Session session = factory.getCurrentSession()) {
+            session.beginTransaction();
+            Student student = session.get(Student.class, id);
+            System.out.println(student);
+            session.getTransaction().commit();
+        }
+    }
+
+    public static void updateStudent(Long id, String newMark) {
+        try (Session session = factory.getCurrentSession()) {
+            session.beginTransaction();
+            Student student = session.get(Student.class, id);
+            student.setMark(newMark);
+            System.out.println(student);
+            session.getTransaction().commit();
+
+        }
+    }
+
+    public static void deleteStudent(Long id) {
+        try (Session session = factory.getCurrentSession()) {
+            Student student = session.get(Student.class, id);
+            if(student == null) {
+                throw new NoSuchElementException("Student with id = " + id + " is not found.");
+            }
+            session.beginTransaction();
+            session.delete(student);
+            session.getTransaction().commit();
+        }
+    }
+
+    public static void shutdown() {
+        factory.close();
+    }
+}
