de92767eb9252a9ff08aa7fb048ddd07cee16a66
KoryakinGG <koryaking@mail.ru>
2021-02-19 16:22:12 +0300

    5 домашка (пару исправлений)

--- src/ru/koryaking/fifthhomework/CrudOperation.java
+++ src/ru/koryaking/fifthhomework/CrudOperation.java
@@ -14,9 +14,10 @@ public class CrudOperation {
     public static void main(String[] args) {
         try {
             init();
-//            createStudent("Mark", "Dark");
-//            readStudent(4L);
-
+            createStudent("Mark", "Dark");
+            readStudent(4L);
+            updateStudent(4L, "Bruce","Batman");
+            deleteStudent(3L);
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -37,38 +38,36 @@ public class CrudOperation {
             Student student = new Student(name, mark);
             session.save(student);
             session.getTransaction().commit();
-            System.out.println("Student create " + student);
+            System.out.println("CREATE: " + student);
         }
     }
-
 
     public static void readStudent(Long id) {
         try (Session session = factory.getCurrentSession()) {
             session.beginTransaction();
             Student student = session.get(Student.class, id);
-            System.out.println(student);
+            System.out.println("READ: " + student);
             session.getTransaction().commit();
         }
     }
 
-    public static void updateStudent(Long id, String newMark) {
+    public static void updateStudent(Long id, String name ,String newMark) {
         try (Session session = factory.getCurrentSession()) {
             session.beginTransaction();
             Student student = session.get(Student.class, id);
             student.setMark(newMark);
-            System.out.println(student);
+            System.out.println("UPDATE: " + student);
             session.getTransaction().commit();
-
         }
     }
 
     public static void deleteStudent(Long id) {
         try (Session session = factory.getCurrentSession()) {
+            session.beginTransaction();
             Student student = session.get(Student.class, id);
-            if(student == null) {
+            if (student == null) {
                 throw new NoSuchElementException("Student with id = " + id + " is not found.");
             }
-            session.beginTransaction();
             session.delete(student);
             session.getTransaction().commit();
         }
